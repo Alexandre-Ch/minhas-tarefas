f4453080f257e3900878e73d215793176b404b16
ale <alecardoso.dev@gmail.com>
2026-06-26 15:28:21 -0300

    Entrega do projeto

---
+++ src/containers/Formulario/index.tsx
@@ -0,0 +1,73 @@
+import { FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { BotaoSalvar, MainContainer, Titulo } from '../../styles'
+import { Campo } from '../../styles'
+import { Form, Opcoes, Opcao } from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+import { cadastrar } from '../../store/reducers/tarefas'
+
+const Formulario = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [titulo, setTitulo] = useState('')
+  const [descricao, setDescricao] = useState('')
+  const [prioridade, setPrioridade] = useState(enums.Prioridade.NORMAL)
+
+  const cadastrarTarefa = (evento: FormEvent) => {
+    evento.preventDefault()
+
+    dispatch(
+      cadastrar({
+        titulo,
+        prioridade,
+        descricao,
+        status: enums.Status.PENDENTE
+      })
+    )
+    navigate('/')
+  }
+
+  return (
+    <MainContainer>
+      <Titulo>Nova Tarefa</Titulo>
+      <Form onSubmit={cadastrarTarefa}>
+        <Campo
+          type="text"
+          placeholder="Título"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        />
+        <Campo
+          as="textarea"
+          placeholder="Descrição"
+          value={descricao}
+          onChange={({ target }) => setDescricao(target.value)}
+        />
+        <Opcoes>
+          <p>Prioridade</p>
+          {Object.values(enums.Prioridade).map((p) => (
+            <Opcao key={p}>
+              <input
+                value={p}
+                name="prioridade"
+                type="radio"
+                onChange={(evento) =>
+                  setPrioridade(evento.target.value as enums.Prioridade)
+                }
+                id={p}
+                defaultChecked={p === enums.Prioridade.NORMAL}
+              />{' '}
+              <label htmlFor={p}>{p}</label>
+            </Opcao>
+          ))}
+        </Opcoes>
+        <BotaoSalvar type="submit">Cadastrar</BotaoSalvar>
+      </Form>
+    </MainContainer>
+  )
+}
+
+export default Formulario
